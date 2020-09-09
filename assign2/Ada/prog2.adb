1c3f298b0106c88c7f99e84f226a0a176b5a160f
Brianna Moffett <bmoffett@poobah.cs.nmsu.edu>
2020-09-08 19:33:40 -0600

    Added shell script (does not work)Added shell script (does not work)Added shell script (does not work)Added shell script (does not work)Added shell script (does not work)Added shell script (does not work)Added shell script (does not work)Added shell script (does not work)Added shell script (does not work)

--- assign2/Ada/prog2.adb
+++ assign2/Ada/prog2.adb
@@ -15,67 +15,38 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure prog2 is
 	A: Integer;
-	function evaluate return Integer is --function to see if boolean is fully evaluated
+	function evaluate return Boolean is --function to see if boolean is fully evaluated
 	begin
 		Put_Line("Condition has been evaluated.");
-		return 1;
+		return true;
 	end evaluate; --end evaluate
 begin
 	A := 1; --Set A to true
 	Put_Line("Testing with and then (false and then true)");
-	if A=0 and then evaluate=1 then --false then true
+	if evaluate=false and then A=1 then --false then true
 		Put_Line("True");
 	else
 		Put_Line("False");
 	end if;--end first test
 	Put_Line("");
 	Put_Line("Testing with and then (true and then false)");
-	if A=1 and then evaluate=0 then --true then false
+	if evaluate=true and then A=0 then --true then false
 		Put_Line("True");
 	else
 		Put_Line("False");
 	end if;--end second test
 	Put_Line("");
 	Put_Line("Testing with and then (true and then true)");
-	if A=1 and then evaluate=1 then --true then true
+	if evaluate=true and then A=1 then --true then true
 		Put_Line("True");
 	else 
 		Put_Line("False");
 	end if;--end third test
 	Put_Line("");
 	Put_Line("Testing with and then (false and then false)");
-	if A=0 and then evaluate=0 then --false then false
+	if evaluate=false and then A=0 then --false then false
 		Put_Line("True");
 	else 
 		Put_Line("False");
 	end if;--end fourth test
-	Put_Line("");
-	Put_Line("Testing with or else (false or else true)");
-	if A=0 or else evaluate=1 then --false then true
-		Put_Line("True");
-	else 
-		Put_Line("False");
-	end if;--end fifth test
-	Put_Line("");
-	Put_Line("Testing with or else (true or else false)");
-	if A=1 or else evaluate=0 then --true then false
-		Put_Line("True");
-	else
-		Put_Line("False");
-	end if;--end sixth test
-	Put_Line("");
-	Put_Line("Testing with or else (true or else true)");
-	if A=1 or else evaluate=1 then --true then true
-		Put_Line("True");
-	else 
-		Put_Line("False");
-	end if;--end seventh test
-	Put_Line("");
-	Put_Line("Testing with or else (false or else false)");
-	if A=0 or else evaluate=0 then --false then false
-		Put_Line("True");
-	else
-		Put_Line("False");
-	end if;--end eigth test
-	Put_Line("");
 end prog2;
--- assign2/shell/prog2.sh
+++ assign2/shell/prog2.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-echo "Beginning experiments...\n"
+echo "Beginning experiments..."
 
-A = 1
-if [$A == 0 -a $?  == 1]
+A=1
+
+if [[ $A == 0 ]] && echo "visited" #false then true
+then
+	echo "True"
+else
+	echo "False"
+fi
+if [[ $A == 1 ]] && !~echo "visited" #true then false
+then
+	echo "True"
+else
+	echo "False"
+fi
+if [[ $A == 1 ]] && echo "visited" #true then true
+then
+	echo "True"
+else
+	echo "False"
+fi
+if [[ $A == 0 ]] && !~echo "visited" #false then false
 then
 	echo "True"
 else
